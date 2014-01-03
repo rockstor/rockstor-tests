289224d8c04475ef26c464c1af2e6a021be8789b
priyaganti <ganti.priya@gmail.com>
2014-01-02 17:57:54 -0800

    make changes according to new attributes of the product.

--- java/src/main/java/com/rockstor/test/webdriver/AddPoolRaid0with5Disks.java
+++ java/src/main/java/com/rockstor/test/webdriver/AddPoolRaid0with5Disks.java
@@ -1,5 +1,10 @@
+package com.rockstor.test.webdriver;
 import java.io.File;
 import java.io.FileInputStream;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,98 +15,115 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.io.FileUtils; // Screenshots
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot; 
+
+import com.rockstor.test.util.RSProps;
+
 import java.io.IOException;
 import java.util.Properties;
 
 
 public class AddPoolRaid0with5Disks {
 
-	public static void main(String[] args) throws IOException {
-		// Create a new instance of the Firefox driver
-		
-		WebDriver driver = new FirefoxDriver();
+	private static WebDriver driver;
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void testPool() throws Exception {
 		try{
 
-			Properties prop = new Properties();
-			prop.load(new FileInputStream("config.properties"));
-			driver.get(prop.getProperty("RockstorVm"));
+			driver.get(RSProps.getProperty("RockstorVm"));
 
-
-			// User  Login Input Forms
-			WebElement username = driver.findElement(By.id("inputUsername"));
+			//User Login Input Forms
+			WebElement username = driver.findElement(
+					By.id("username"));
 			username.sendKeys("admin");
 
-			WebElement password = driver.findElement(By.id("inputPassword"));
+			WebElement password = driver.findElement(
+					By.id("password"));
 			password.sendKeys("admin");
 
-			WebElement submit = driver.findElement(By.id("sign_in"));
-			submit.click();
-			
-			// Select Pools from Navigation bar
-			WebElement poolsNav = driver.findElement(By.id("pools_nav"));
-			poolsNav.click();
+			WebElement submitButton = driver.findElement(
+					By.id("sign_in"));
+			submitButton.click();
 
-			//Explicit Wait for Pools page to load
+			// Select Storage from Navigation bar
+			WebElement storageNav = driver.findElement(By.id("storage_nav"));
+			storageNav.click();
+
+
+			// Select pools from storage side bar
+			WebElement poolNav = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'pools')]"));
+			poolNav.click();
+
+			//Explicit Wait for Create Pool button to load
 			WebElement myWaitElement1 = (new WebDriverWait(driver, 150))
-					.until(ExpectedConditions.elementToBeClickable(By.id("add_pool")));
-
+					.until(ExpectedConditions.elementToBeClickable(
+							By.id("add_pool")));
 
 			WebElement addPool = driver.findElement(By.id("add_pool"));
 			addPool.click();
 
-			//Explicit Wait for CreatePools page. 
+			//Explicit Wait for Create Pool page. 
 			WebElement myWaitElement2 = (new WebDriverWait(driver, 150))
-					.until(ExpectedConditions.elementToBeClickable(By.id("create_pool")));
+					.until(ExpectedConditions.elementToBeClickable(
+							By.id("create_pool")));
 
 
 			WebElement poolname = driver.findElement(By.id("pool_name"));
 			poolname.sendKeys("pool1");
 
 			//Raid Configuration Dropdown box 
-			Select raidConfigDroplist = new Select(driver.findElement(By.id("raid_level")));   
-			raidConfigDroplist.selectByIndex(0);
-
-			//Select Disks CheckBox
-			WebElement diskCheckBox1 = driver.findElement(By.id("sdb"));
-			diskCheckBox1.click();
-			WebElement diskCheckBox2 = driver.findElement(By.id("sdc"));
-			diskCheckBox2.click();
-			WebElement diskCheckBox3 = driver.findElement(By.id("sdd"));
-			diskCheckBox3.click();
-			WebElement diskCheckBox4 = driver.findElement(By.id("sde"));
-			diskCheckBox4.click();
-			WebElement diskCheckBox5 = driver.findElement(By.id("sdf"));
-			diskCheckBox5.click();
+			Select raidConfigDroplist = new Select(
+					driver.findElement(By.id("raid_level")));   
+			raidConfigDroplist.selectByIndex(1);
 			
+			//Select Disks CheckBox
+            WebElement diskCheckBox1 = driver.findElement(By.id("sdb"));
+            diskCheckBox1.click();
+            WebElement diskCheckBox2 = driver.findElement(By.id("sdc"));
+            diskCheckBox2.click();
+            WebElement diskCheckBox3 = driver.findElement(By.id("sdd"));
+            diskCheckBox3.click();
+            WebElement diskCheckBox4 = driver.findElement(By.id("sde"));
+            diskCheckBox4.click();
+            WebElement diskCheckBox5 = driver.findElement(By.id("sdf"));
+            diskCheckBox5.click();
+            
+            
 
 			//Create Pool
 			WebElement createPool = driver.findElement(By.id("create_pool"));
 			createPool.click();
+			
 
-			WebElement myWaitElement3 = (new WebDriverWait(driver, 150))
-					.until(ExpectedConditions.elementToBeClickable(By.id("delete_pool_pool1")));
+			//Logout
+			WebElement logoutSubmit = driver.findElement(
+					By.id("logout_user"));
+
+			logoutSubmit.click();
+
+
 		}
-
-		//catch any exceptions by taking screenshots.
+		//catch any exceptions by taking screenshots
 		catch(Exception e){
-
-			System.out.println(e.toString());
-
-			File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(screenshotFile,new File("/home/priya/rockstor-tests/webdriver/java/ScreenShots/AddPool_Raid05Disks.png"));
-
+			File screenshotFile = ((TakesScreenshot)driver)
+					.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshotFile,
+					new File(RSProps.getProperty("screenshotDir") 
+							+ "/" + this.getClass().getName()+".png"));
+			throw e;
 		}
-
-		//click on logout
-		WebElement logoutSubmit = driver.findElement(By.id("logout_user"));
-
-		logoutSubmit.click();
-		driver.close();
 
 	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.quit();
+	}
 }
-
 
 
 
