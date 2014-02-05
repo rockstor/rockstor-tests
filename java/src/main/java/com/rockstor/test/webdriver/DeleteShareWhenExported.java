f1b512f7f9d47fb8e67ee5b62b55a7c903c90017
Madhuri Chakravarthula <mchakravartula@gmail.com>
2014-02-05 20:45:23 +0000

    Add Test class test to delete share which is exported vias nfs/samba.

--- java/src/main/java/com/rockstor/test/webdriver/DeleteShareWhenExported.java
+++ java/src/main/java/com/rockstor/test/webdriver/DeleteShareWhenExported.java
@@ -27,8 +27,7 @@ import java.util.List;
 import com.rockstor.test.util.RSProps;
 
 
-public class DeleteShare {
-
+public class DeleteShareWhenExported {
 	private static WebDriver driver;
 
 	@BeforeClass
@@ -38,9 +37,9 @@ public class DeleteShare {
 				Integer.parseInt(RSProps.getProperty("waitTimeout")), 
 				TimeUnit.SECONDS);	
 	}
-	
+
 	@Test
-	public void testDeleteShare() throws Exception {
+	public void testDeleteShareWhenExported() throws Exception {
 		try{
 
 			driver.get(RSProps.getProperty("RockstorVm"));
@@ -55,11 +54,10 @@ public class DeleteShare {
 			WebElement submit = driver.findElement(By.id("sign_in"));
 			submit.click();
 
-			//Create Pool
-
+			
 			WebElement storageNav = driver.findElement(By.id("storage_nav"));
 			storageNav.click();
-
+			
 			// Add Pool with Raid 0
 			WebElement poolsNav = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'pools')]"));
 			poolsNav.click();
@@ -71,7 +69,8 @@ public class DeleteShare {
 			poolname.sendKeys("pool1");
 
 			// Raid Configuration Dropdown box 
-			Select raidConfigDroplist = new Select(driver.findElement(By.id("raid_level")));   
+			Select raidConfigDroplist = new Select(driver.findElement(
+					By.id("raid_level")));   
 			raidConfigDroplist.selectByIndex(0);
 
 			//Select Disks CheckBox
@@ -80,29 +79,28 @@ public class DeleteShare {
 			WebElement diskCheckBox2 = driver.findElement(By.id("sdc"));
 			diskCheckBox2.click();
 
-
 			// Create Pool
 			WebElement createPool = driver.findElement(By.id("create_pool"));
 			createPool.click();
-			
+
 			//wait for pool1 to appear
 			WebElement poolLink = driver.findElement(By.linkText("pool1"));
 			poolLink.click();
 
-			// Create a share
+			///// Create a share
 
 			//Shares navigation bar
 			WebElement sharesNav = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'shares')]"));
 			sharesNav.click();
 
 			//Add share
-			WebElement addShare = driver.findElement(By.id("add_share"));
-			addShare.click();
+			WebElement addShareButton = driver.findElement(By.id("add_share"));
+			addShareButton.click();
 
 			WebElement shareName = driver.findElement(By.id("share_name"));
 			shareName.sendKeys("share1");
 
-			Select selectPoolDroplist = new Select(driver.findElement(By.id("pool_name"))); 
+			Select selectPoolDroplist = new Select(driver.findElement(By.id("pool_name")));   
 			selectPoolDroplist.selectByIndex(0); 
 
 
@@ -111,47 +109,111 @@ public class DeleteShare {
 
 
 			Select selectSizeDroplist = new Select(driver.findElement(By.id("size_format")));   
-			selectSizeDroplist.selectByIndex(0); // 0 is KB
+			selectSizeDroplist.selectByIndex(2); // 2 is GB
 
 
 			//Submit button to create share
 			WebElement shareSubmitButton = driver.findElement(By.id("create_share"));
 			shareSubmitButton.click();
+			
+			//Nfs navigation bar
+			WebElement nfsNav = driver.findElement(By.xpath("//div[@class='subnav']/ul/li/a[contains(@href,'nfs-exports')]"));
+			nfsNav.click();
+			
+			
+			// Add nfs-export
+			WebElement addNfsButton = driver.findElement(By.id("add-nfs-export"));
+			addNfsButton.click();
+			
+			
+			Select selectShareDroplist = new Select(driver.findElement(By.id("shares")));   
+			selectShareDroplist.selectByIndex(0);
+			
+			
+			
+			WebElement hostString = driver.findElement(By.id("host_str"));
+			hostString.sendKeys("rockstor.com");
+			
+			//Select Mode
+			Select selectModDroplist = new Select(driver.findElement(By.id("mod_choice")));   
+			selectModDroplist.selectByIndex(0);
+			
+			//Select Sync Option
+			Select selectSyncDroplist = new Select(driver.findElement(By.id("sync_choice")));   
+			selectSyncDroplist.selectByIndex(0);
+			
+			//Submit button to create nfs export
+			WebElement nfsCreateButton = driver.findElement(By.id("create-nfs-export"));
+			nfsCreateButton.click();
+			
+			//Shares navigation bar
+			WebElement sharesNav1 = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'shares')]"));
+			sharesNav1.click();
+			
+			// Delete Share
 
-
-           //Delete share
 			
 			WebElement shareRow = driver.findElement(By.xpath("//*[@id='shares-table']/tbody/tr[td[contains(.,'share1')]]"));
 			WebElement deleteShare = shareRow.findElement(By.className("icon-trash"));
 			deleteShare.click();
 
-
+			
 			//Browser Popup asking confirmation to delete 
 			Alert alertDeleteShare = driver.switchTo().alert();
 			alertDeleteShare.accept();
+			
+			//check for the text
+			WebElement verifyErrorMsg = driver.findElement(By.cssSelector("div[class$='alert-error']"));
+			assertTrue(verifyErrorMsg.getText().contains("Error"));
+			
+			//Delete Error message
+			WebElement closeErrorButton = driver.findElement(By.className("close"));
+			closeErrorButton.click();
+			
+            
+			//Nfs navigation bar
+			WebElement nfsNav1 = driver.findElement(By.xpath("//div[@class='subnav']/ul/li/a[contains(@href,'nfs-exports')]"));
+			nfsNav1.click();
+			
+			//Delete nfs export
+			WebElement nfsRow = driver.findElement(By.xpath("//*[@id='nfs-exports-table']/tbody/tr[td[contains(.,'rockstor.com')]]"));
+			WebElement deleteNfs = nfsRow.findElement(By.className("icon-trash"));
+			deleteNfs.click();
+			
+			//Browser Popup asking confirmation to delete 
+			Alert alertDeleteNfs = driver.switchTo().alert();
+			alertDeleteNfs.accept();
+			
+			//Shares navigation bar
+			WebElement sharesNav2 = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'shares')]"));
+			sharesNav2.click();
+			
+			// Delete Share
 
 			
-			// verify if all shares are deleted
-			WebElement verifyTextPresent = driver.findElement(
-					By.xpath("//div/h4[text()='No shares have been created']"));
-			assertTrue(verifyTextPresent.getText(), true);
+			WebElement shareRow1 = driver.findElement(By.xpath("//*[@id='shares-table']/tbody/tr[td[contains(.,'share1')]]"));
+			WebElement deleteShare1 = shareRow1.findElement(By.className("icon-trash"));
+			deleteShare1.click();
+			
+			//Browser Popup asking confirmation to delete 
+			Alert alertDeleteShare1 = driver.switchTo().alert();
+			alertDeleteShare1.accept();
 			
 			// Delete Pool
 			WebElement poolsNav1 = driver.findElement(By.xpath("//div[@id='sidebar-inner']/ul/li/a[contains(@href,'pools')]"));
 			poolsNav1.click();
-
+			
 			WebElement poolRow = driver.findElement(By.xpath("//*[@id='pools-table']/tbody/tr[td[contains(.,'pool1')]]"));
 			WebElement deletePool = poolRow.findElement(By.className("icon-trash"));
 			deletePool.click();
 
-
 			//Browser Popup asking confirmation to delete 
 			Alert alertDeletePool = driver.switchTo().alert();
 			alertDeletePool.accept();
-
-
+			
 			// Logout 
 			WebElement logoutSubmit = driver.findElement(By.id("logout_user"));
+
 			logoutSubmit.click();
 
 		}
--- java/src/main/java/com/rockstor/test/webdriver/SharesTestSuite.java
+++ java/src/main/java/com/rockstor/test/webdriver/SharesTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({LinkToSharePage.class,DeleteShare.class,ListSharesWithSizes.class,
+@SuiteClasses({LinkToSharePage.class,DeleteShareWhenExported.class,ListSharesWithSizes.class,
 	MultipleSharesinSamePool.class,})
 
 public class SharesTestSuite {
